9cf5995ddf85434dbc60273b72de12f2fa6ef539
Xavier <xmtrinidad@gmail.com>
2022-04-09 09:13:16 -0700

    more basic examples

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var memeName = "Meme Man"
+	const totalMemes = 50
+	var remainingMemes = 10
+
+	fmt.Println("Welcome", memeName, "you have made", totalMemes, "total memes.")
+	fmt.Println("There are still", remainingMemes, "memes to make.")
+	fmt.Println()
+
+	// Another way to write the above println statements
+	fmt.Printf("Wecome %v you have made %v total memes\n", memeName, totalMemes)
+	fmt.Printf("There are still %v memes to make.\n", remainingMemes)
 }
